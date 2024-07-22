c5d0c35e5ed7cc0e0fb95aea1819c794710e90de
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-22 16:57:50 +0200

    feat:  Add slice maschine

--- apps/kids-guitar-dojo/src/app/page.tsx
+++ apps/kids-guitar-dojo/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome kids-guitar-dojo 👋
+              Welcome Kids Guitar Dojo 👋
             </h1>
           </div>
 
