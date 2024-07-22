d4cdf80670bff52ad8769b1cfc9457589fe82cf4
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-22 08:52:18 +0200

    Initial commit

---
+++ apps/rocket_house_productions/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'rocket_house_productions',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/rocket_house_productions',
+};
