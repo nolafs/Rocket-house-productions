19d8e21a3c8ddcd1c4d9f3bcfe892693fb29aabc
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-25 19:45:01 +0200

    feat:  Blog, Legal page
    - header simple setup for now

--- libs/shared/types/src/index.ts
+++ libs/shared/types/src/index.ts
@@ -1,6 +1,6 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-export {NavigationProps} from './lib/navigation.type';
-export {SocialLinkItemType} from './lib/socialLinkItem.type';
-export {LinkPrismicType} from './lib/link.prismic.type';
-export {BlogType} from './lib/bog.type';
-export {BlogPostType} from './lib/bog.type';
+export type {NavigationProps} from './lib/navigation.type';
+export type {SocialLinkItemType} from './lib/socialLinkItem.type';
+export type {LinkPrismicType} from './lib/link.prismic.type';
+export type {BlogType} from './lib/bog.type';
+export type {BlogPostType} from './lib/bog.type';
