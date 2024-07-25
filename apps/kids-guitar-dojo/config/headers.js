dcf9bdac759044a5c3a2f89e0a9fc9157ce3daa6
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-25 19:22:03 +0200

    feat:  Blog, Legal page
    - header simple setup for now

--- apps/kids-guitar-dojo/config/headers.js
+++ apps/kids-guitar-dojo/config/headers.js
@@ -7,17 +7,18 @@ const securityHeaders = [
     key: 'Strict-Transport-Security',
     value: 'max-age=63072000; includeSubDomains; preload',
   },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
   /*
-  {
-    key: 'Content-Security-Policy',
-    value: `frame-ancestors 'self' https://app.contentful.com https://app.eu.contentful.com`,
-  },
+{
+  key: 'X-Frame-Options',
+  value: 'SAMEORIGIN',
+},
 
-   */
+{
+  key: 'Content-Security-Policy',
+  value: `frame-ancestors 'self' https://app.contentful.com https://app.eu.contentful.com`,
+},
+
+ */
   {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
