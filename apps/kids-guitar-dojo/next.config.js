7e7f4822186a8f9562e9620dcb11ca7dbc27ff7c
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-26 14:18:52 +0200

    feat: Content block with image

--- apps/kids-guitar-dojo/next.config.js
+++ apps/kids-guitar-dojo/next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
-  ...pluginsExtends,
+  //...pluginsExtends,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
