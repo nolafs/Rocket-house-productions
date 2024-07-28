75725a7520fa1a18eb7512b1cc6e2021dac9649c
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-28 13:40:25 +0200

    feature:  add Shadcn ui setup

--- libs/shared/features/src/index.ts
+++ libs/shared/features/src/index.ts
@@ -1,9 +1,9 @@
-export * from './lib/newsletter-form/newsletter-form-status';
-export * from './lib/newsletter-form/newsletter-form';
+export { NewsletterFormStatus } from './lib/newsletter-form/newsletter-form-status';
+export { NewsletterForm } from './lib/newsletter-form/newsletter-form';
 export { HeroColumns } from './lib/hero-header/hero-columns/hero-columns';
 export * from './lib/motto-text/motto-text';
 export * from './lib/section-title/section-title';
-export * from './lib/content-block-image/content-block-image';
+export { ContentBlockImage } from './lib/content-block-image/content-block-image';
 export { Breadcrumb } from './lib/breadcumbs/Breadcrumb';
 export * from './lib/social-list/social-list';
 export { BlogPost } from './lib/blog/blog-post';
