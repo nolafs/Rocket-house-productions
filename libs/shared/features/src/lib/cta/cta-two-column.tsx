e7a9b500c5e4603936f50e3528cacf1f0f180f92
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-29 12:41:56 +0200

    feat: CTA

--- libs/shared/features/src/lib/cta/cta-two-column.tsx
+++ libs/shared/features/src/lib/cta/cta-two-column.tsx
@@ -14,8 +14,8 @@ interface HeroColumnsProps {
 
 export function CtaTwoColumn({ data: { headings, buttons, motto }, color }: HeroColumnsProps) {
   return (
-    <div className="rounded bg-gray-900">
-      <div className="grid px-6 py-24 sm:py-24 md:grid-cols-2 md:px-10 lg:items-center lg:justify-between lg:px-20">
+    <div className="rounded-md bg-gray-900">
+      <div className="grid px-6 py-16 sm:py-20 md:grid-cols-2 md:px-10 lg:items-center lg:justify-between lg:px-20">
         <div>
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
             <PrismicRichText field={headings} />
--- libs/shared/shadcn-ui/src/components/ui/button.tsx
+++ libs/shared/shadcn-ui/src/components/ui/button.tsx
@@ -9,9 +9,9 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground  hover:bg-primary/20 hover:text-primary',
+        default: 'bg-primary text-primary-foreground  hover:bg-accent hover:text-primary',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-primary text-primary bg-background hover:bg-accent hover:text-accent-foreground',
+        outline: 'border border-primary text-primary bg-background hover:bg-primary hover:text-primary-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
