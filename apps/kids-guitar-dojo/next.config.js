04fb6375fe385409cfaa1cd961fbbecc1702c868
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-31 13:02:34 +0200

    feat: custom sign in pages

--- apps/kids-guitar-dojo/next.config.js
+++ apps/kids-guitar-dojo/next.config.js
@@ -1,7 +1,7 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-
+const path = require('path');
 const { composePlugins, withNx } = require('@nx/next');
 const headers = require('./config/headers');
 const pluginsExtends = require('./config/plugins');
@@ -13,6 +13,12 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: true,
+  },
+  output: 'standalone',
+  // https://nextjs.org/docs/advanced-features/output-file-tracing#caveats
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
   },
   //...(process.env.ENVIRONMENT_NAME !== 'local' && {headers}),
   images: {
