60b7e300e9a9208868951a4479dd549d9d4b0101
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-31 12:20:24 +0200

    feat: custom sign in pages

--- apps/kids-guitar-dojo/src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ apps/kids-guitar-dojo/src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,14 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <main>
+      <div className={'flex h-svh min-h-svh w-full flex-col justify-center md:flex-row'}>
+        <div className={'bg-primary md:w-1/2'}></div>
+        <div className={'flex w-full items-center justify-center bg-white md:w-1/2'}>
+          <SignIn />
+        </div>
+      </div>
+    </main>
+  );
 }
