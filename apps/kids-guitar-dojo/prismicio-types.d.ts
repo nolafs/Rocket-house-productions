42e61ee85b7a949585bce0e34c938217605c6635
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-31 19:12:28 +0200

    feat: Section Stats added

--- apps/kids-guitar-dojo/prismicio-types.d.ts
+++ apps/kids-guitar-dojo/prismicio-types.d.ts
@@ -564,6 +564,7 @@ export type NavigationDocument<Lang extends string = string> = prismic.PrismicDo
 >;
 
 type PageDocumentDataSlicesSlice =
+  | TestimonialsSlice
   | StatsSlice
   | TimelineSlice
   | FeaturesSlice
