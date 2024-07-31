28baef56c47231fb5c2a5af64d11cec25ad7560a
Olaf Siebert <olaf@biffnewmedia.com>
2024-07-31 12:48:49 +0200

    feat: custom sign in pages

--- apps/kids-guitar-dojo/src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ apps/kids-guitar-dojo/src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main>
       <div className={'flex h-svh min-h-svh w-full flex-col justify-center md:flex-row'}>
-        <div className={'bg-primary md:w-1/2'}></div>
+        <div className={'bg-primary md:w-1/2'}>Some CTA here</div>
         <div className={'flex w-full items-center justify-center bg-white md:w-1/2'}>
           <SignIn />
         </div>
