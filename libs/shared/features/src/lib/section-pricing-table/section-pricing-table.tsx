5285e50e17192b42def3fdfa07f80624fd97042e
Olaf Siebert <olaf@biffnewmedia.com>
2024-08-01 09:45:32 +0200

    feat: Add Pricing table

--- libs/shared/features/src/lib/section-pricing-table/section-pricing-table.tsx
+++ libs/shared/features/src/lib/section-pricing-table/section-pricing-table.tsx
@@ -1,8 +1,9 @@
 import cn from 'classnames';
-import { CheckIcon } from 'lucide-react';
+import { CheckCircleIcon } from 'lucide-react';
 import { KeyTextField, RichTextField } from '@prismicio/client';
 import { PrismicRichText } from '@prismicio/react';
 import { PrismicNextLink } from '@prismicio/next';
+import { buttonVariants } from '@rocket-house-productions/shadcn-ui';
 
 interface SectionPricingTableProps {
   tiers: {
@@ -13,7 +14,7 @@ interface SectionPricingTableProps {
       description: RichTextField | null | undefined;
       price: any;
       features: { feature: string }[];
-      mostPopular?: boolean;
+      most_popular?: boolean;
       link: any;
     };
   }[];
@@ -24,46 +25,53 @@ export function SectionPricingTable({ tiers }: SectionPricingTableProps) {
     return null;
   }
 
+  console.log('tiers', tiers);
+
   return (
     <div className="isolate mx-auto grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       {tiers.map((tier, idx) => (
         <div
           key={tier.id}
           className={cn(
-            tier.data.mostPopular ? 'ring-2 ring-indigo-600' : 'ring-1 ring-gray-200',
-            'rounded-3xl p-8 xl:p-10',
+            tier.data.most_popular ? 'ring-primary ring-2' : 'ring-1 ring-gray-200',
+            'rounded bg-[#f7f8f9] p-8 xl:p-10',
           )}>
           <div className="flex items-center justify-between gap-x-4">
             <h3
               className={cn(
-                tier.data.mostPopular ? 'text-indigo-600' : 'text-gray-900',
-                'text-lg font-semibold leading-8',
+                tier.data.most_popular ? 'text-primary' : 'text-gray-900',
+                'text-2xl font-semibold leading-8',
               )}>
               {tier.data.name}
             </h3>
-            {tier.data.mostPopular ? (
-              <p className="rounded-full bg-indigo-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-indigo-600">
+            {tier.data.most_popular ? (
+              <p className="bg-primary rounded-full px-2.5 py-1 text-xs font-semibold leading-5 text-white">
                 Most popular
               </p>
             ) : null}
           </div>
-          <div className="mt-4 text-sm leading-6 text-gray-600">
+          <div className="mt-4 leading-6 text-gray-600">
             <PrismicRichText field={tier.data.description} />
           </div>
           <PrismicNextLink
             field={tier.data.link}
             className={cn(
-              tier.data.mostPopular
-                ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500'
-                : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300',
-              'mt-6 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+              buttonVariants({
+                variant: tier.data.most_popular ? 'default' : 'outline',
+                size: 'lg',
+                className: 'mt-6 w-full shadow-sm shadow-black/30',
+              }),
             )}>
-            Buy Now
+            Start now
           </PrismicNextLink>
           <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-600 xl:mt-10">
             {tier.data.features.map((item, idx) => (
               <li key={tier.id + 'feature' + idx} className="flex gap-x-3">
-                <CheckIcon aria-hidden="true" className="h-6 w-5 flex-none text-indigo-600" />
+                <CheckCircleIcon
+                  aria-hidden="true"
+                  fill={'rgb(44, 103, 221)'}
+                  className="h-6 w-5 flex-none text-white"
+                />
                 {item.feature}
               </li>
             ))}
