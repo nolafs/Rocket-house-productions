3b8fdd8f3541f904c3a58183840c7bc244673f06
Olaf Siebert <olaf@biffnewmedia.com>
2024-08-04 16:30:06 +0200

    core:  lint cleanup

--- apps/kids-guitar-dojo/src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ apps/kids-guitar-dojo/src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,22 @@
 import { SignUp } from '@clerk/nextjs';
+import Image from 'next/image';
+import LogoFull from '@assets/logo_full.png';
+import { PrismicNextImage } from '@prismicio/next';
+import { createClient } from '@/prismicio';
 
-export default function Page() {
+export default async function Page() {
+  const client = createClient();
+  const settings = await client.getSingle('settings');
   return (
     <main>
       <div className={'flex h-svh min-h-svh w-full flex-col justify-center md:flex-row'}>
-        <div className={'bg-primary md:w-1/2'}></div>
-        <div className={'flex w-full items-center justify-center bg-white md:w-1/2'}>
+        <div className={'bg-primary flex flex-col items-center justify-center md:w-1/2'}>
+          <PrismicNextImage field={settings.data.sign_up_image} />
+        </div>
+        <div className={'flex w-full flex-col items-center justify-center bg-white md:w-1/2'}>
+          <div>
+            <Image src={LogoFull} alt={'Kids Guitar Dojo'} width={112} height={28} />
+          </div>
           <SignUp />
         </div>
       </div>
