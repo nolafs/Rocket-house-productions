57b1678598302cfc41ee5240d428a850e61d0513
Olaf Siebert <olaf@biffnewmedia.com>
2024-08-29 19:12:15 +0200

    feat:  update debug menu

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(lesson)/[slug]/(course)/_component/course-debug-navigation.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(lesson)/[slug]/(course)/_component/course-debug-navigation.tsx
@@ -16,14 +16,17 @@ interface CourseDebugNavigationProps {
 
 export function CourseDebugNavigation({ course }: CourseDebugNavigationProps) {
   return (
-    <div className={'fex-col flex items-center justify-center rounded-xl bg-white p-10'}>
-      <div className={'flex flex-col space-y-3'}>
+    <div
+      className={
+        'fex-col flex max-h-[500px] min-w-[640px] items-center justify-center overflow-hidden rounded-xl bg-white p-10'
+      }>
+      <div className={'flex w-full flex-col space-y-3'}>
         <h1 className={'text-primary flex flex-col text-2xl font-bold'}>
           <small className={'text-sm text-black'}>Course Debug menu</small>
           {course.title}
         </h1>
-        <div className={'grid grid-cols-2 gap-5'}>
-          <div className={'prose prose-sm max-w-5xl rounded-md border p-5'}>
+        <div className={'flex max-h-[480px] w-full gap-5'}>
+          <div className={'prose prose-sm max-w-5xl shrink p-5'}>
             {course.description && <div dangerouslySetInnerHTML={{ __html: course.description }}></div>}
             <div className={'mb-4 font-bold'}>Attachments</div>
             <ul className={'space-y-3 text-sm'}>
@@ -36,89 +39,90 @@ export function CourseDebugNavigation({ course }: CourseDebugNavigationProps) {
               ))}
             </ul>
           </div>
-
-          <Accordion type="single" collapsible className="w-full">
-            {course.modules.map((module: any, idx: number) => (
-              <AccordionItem value={module.id} key={module.id}>
-                <AccordionTrigger>
-                  {idx + 1} {module.title}{' '}
-                  <span
-                    className={'inline-block h-6 w-6 rounded-full'}
-                    style={{ backgroundColor: module.color || 'transparent' }}></span>
-                </AccordionTrigger>
-                <AccordionContent>
-                  <div className={'flex flex-col space-y-3'}>
-                    <div>
-                      <div className={'mb-4 font-bold'}>Description:</div>
-                      <div dangerouslySetInnerHTML={{ __html: module.description || '' }}></div>
-                    </div>
-                    <div>
-                      <div className={'mb-4 font-bold'}>Lessons:</div>
-                      <ul className={'space-y-3 text-sm'}>
-                        {module.lessons.map((lesson: any, idx: number) => (
-                          <li key={lesson.id}>
-                            <Link
-                              className={cn(
-                                buttonVariants({ variant: 'default' }),
-                                'w-full',
-                                lesson.category?.name === 'Dr Rhythm' && '!bg-pink-900',
-                                lesson?.category?.name === 'Practice' && '!bg-gray-500',
-                              )}
-                              href={`/courses/${course.slug}/modules/${module.slug}/lessons/${lesson.slug}`}>
-                              {idx + 1}: {lesson.category?.name} - {lesson.title}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                    <div>
-                      <div className={'mb-4 font-bold'}>Attachments</div>
-                      <ul className={'space-y-3 text-sm'}>
-                        {module.attachments.map((attachment: any) => (
-                          <li key={attachment.id} className={'flex flex-col space-y-1.5 rounded-md border p-3'}>
-                            <div>Type: {attachment.attachmentType?.name}</div>
-                            <div>Name: {attachment.name}</div>
-                            <ButtonDownloadPdf url={attachment.url} filename={attachment.name || 'download'} />
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                    <div>
+          <div className={'max-h-[342px] overflow-y-auto'}>
+            <Accordion type="single" collapsible className="w-full">
+              {course.modules.map((module: any, idx: number) => (
+                <AccordionItem value={module.id} key={module.id}>
+                  <AccordionTrigger>
+                    {idx + 1} {module.title}{' '}
+                    <span
+                      className={'inline-block h-6 w-6 rounded-full'}
+                      style={{ backgroundColor: module.color || 'transparent' }}></span>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className={'flex flex-col space-y-3'}>
                       <div>
-                        <div className={'mb-4 font-bold'}>Awards</div>
-                        <ul>
-                          {module.availableAwards.map((item: any, idx: number) => {
-                            return (
-                              <li key={'award' + item.awardType.id} className={'flex space-x-1.5'}>
-                                {item.awardType.badgeUrl && (
-                                  <Image src={item.awardType.badgeUrl} width={70} height={70} alt={'badge'} />
-                                )}{' '}
-                                <span>{item.awardType.name}</span>
-                              </li>
-                            );
-                          })}
+                        <div className={'mb-4 font-bold'}>Description:</div>
+                        <div dangerouslySetInnerHTML={{ __html: module.description || '' }}></div>
+                      </div>
+                      <div>
+                        <div className={'mb-4 font-bold'}>Lessons:</div>
+                        <ul className={'space-y-3 text-sm'}>
+                          {module.lessons.map((lesson: any, idx: number) => (
+                            <li key={lesson.id}>
+                              <Link
+                                className={cn(
+                                  buttonVariants({ variant: 'default' }),
+                                  'w-full',
+                                  lesson.category?.name === 'Dr Rhythm' && '!bg-pink-900',
+                                  lesson?.category?.name === 'Practice' && '!bg-gray-500',
+                                )}
+                                href={`/courses/${course.slug}/modules/${module.slug}/lessons/${lesson.slug}`}>
+                                {idx + 1}: {lesson.category?.name} - {lesson.title}
+                              </Link>
+                            </li>
+                          ))}
                         </ul>
                       </div>
-                    </div>
-                    <div>
                       <div>
-                        <div className={'mb-4 font-bold'}>Quiz</div>
-                        <ul>
-                          {module.lessons.map((item: any, idx: number) => {
-                            return (
-                              <li key={'quiz' + item.id}>
-                                {idx + 1} {item.title} - {item.questionaries.length ? 'yes' : 'no'}
-                              </li>
-                            );
-                          })}
+                        <div className={'mb-4 font-bold'}>Attachments</div>
+                        <ul className={'space-y-3 text-sm'}>
+                          {module.attachments.map((attachment: any) => (
+                            <li key={attachment.id} className={'flex flex-col space-y-1.5 rounded-md border p-3'}>
+                              <div>Type: {attachment.attachmentType?.name}</div>
+                              <div>Name: {attachment.name}</div>
+                              <ButtonDownloadPdf url={attachment.url} filename={attachment.name || 'download'} />
+                            </li>
+                          ))}
                         </ul>
                       </div>
+                      <div>
+                        <div>
+                          <div className={'mb-4 font-bold'}>Awards</div>
+                          <ul>
+                            {module.availableAwards.map((item: any, idx: number) => {
+                              return (
+                                <li key={'award' + item.awardType.id} className={'flex space-x-1.5'}>
+                                  {item.awardType.badgeUrl && (
+                                    <Image src={item.awardType.badgeUrl} width={70} height={70} alt={'badge'} />
+                                  )}{' '}
+                                  <span>{item.awardType.name}</span>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                      </div>
+                      <div>
+                        <div>
+                          <div className={'mb-4 font-bold'}>Quiz</div>
+                          <ul>
+                            {module.lessons.map((item: any, idx: number) => {
+                              return (
+                                <li key={'quiz' + item.id}>
+                                  {idx + 1} {item.title} - {item.questionaries.length ? 'yes' : 'no'}
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
         </div>
       </div>
     </div>
