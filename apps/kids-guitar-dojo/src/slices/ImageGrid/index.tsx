cfac771618266de294b79460b0068735a38b24c8
Olaf Siebert <olaf@biffnewmedia.com>
2024-08-29 13:01:59 +0200

    fix: image grid layout fix

--- apps/kids-guitar-dojo/src/slices/ImageGrid/index.tsx
+++ apps/kids-guitar-dojo/src/slices/ImageGrid/index.tsx
@@ -14,10 +14,10 @@ const ImageGrid = ({ slice }: ImageGridProps): JSX.Element => {
   return (
     <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
       <div
-        className={`grid grid-cols-1 gap-4 sm:grid-cols-${slice.primary.no_columns_small || 1} md:grid-cols-${slice.primary.no_columns_medium || 2} lg:grid-cols-${slice.primary.no_columns_large || 3}`}>
+        className={`mb-8 grid grid-cols-1 gap-4 sm:grid-cols-${slice.primary.no_columns_small || 1} md:grid-cols-${slice.primary.no_columns_medium || 2} lg:grid-cols-${slice.primary.no_columns_large || 3}`}>
         {slice.primary.items.map((item, index) => {
           return (
-            <div key={index} className="relative">
+            <div key={index} className="relative overflow-hidden">
               <PrismicNextImage field={item.image} className="h-full w-full object-cover" />
               {item.caption && (
                 <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
