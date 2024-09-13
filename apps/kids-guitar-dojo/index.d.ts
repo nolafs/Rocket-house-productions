9c5e88a39386c88ac98f872fa747dc04c0b4a05a
Olaf Siebert <olaf@biffnewmedia.com>
2024-09-13 19:48:25 +0200

    feat: 3d clouds added

--- apps/kids-guitar-dojo/index.d.ts
+++ apps/kids-guitar-dojo/index.d.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactThreeFiber } from '@react-three/fiber';
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+
 declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
@@ -10,3 +13,12 @@ declare module 'server-only';
 declare module 'player.js';
 
 declare module 'three.meshline';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      MeshLineGeometry: ReactThreeFiber.Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+      MeshLineMaterial: ReactThreeFiber.Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
+    }
+  }
+}
