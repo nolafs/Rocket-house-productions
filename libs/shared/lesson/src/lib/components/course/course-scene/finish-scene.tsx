2bf14f153bf048ae2e7d4e9df83717f309efa94c
Olaf Siebert <olaf@biffnewmedia.com>
2024-09-14 10:48:50 +0200

    feat: calculate pages for scroll

--- libs/shared/lesson/src/lib/components/course/course-scene/finish-scene.tsx
+++ libs/shared/lesson/src/lib/components/course/course-scene/finish-scene.tsx
@@ -14,7 +14,11 @@ export const FinalScene = ({ position, rotation, pathLength = 0, ...rest }: Fina
 
   return (
     <group ref={ref}>
-      <mesh geometry={new PlaneGeometry(15, 18)} position={[0, pathLength + 15.8, -25]} scale={1} rotation={[0, 0, 0]}>
+      <mesh
+        geometry={new PlaneGeometry(15, 18)}
+        position={[0, pathLength + 15.8, -24.5]}
+        scale={1}
+        rotation={[0, 0, 0]}>
         <meshStandardMaterial map={fred} color={0xffffff} transparent={true} metalness={0.4} />
       </mesh>
     </group>
