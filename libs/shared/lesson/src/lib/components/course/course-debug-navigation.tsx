0da7737877d4a8bf64831496c6bb968f6c003137
Olaf Siebert <olaf@biffnewmedia.com>
2024-09-16 11:33:06 +0200

    feat:  add mobile back button
    - make sure white background for lessons

--- libs/shared/lesson/src/lib/components/course/course-debug-navigation.tsx
+++ libs/shared/lesson/src/lib/components/course/course-debug-navigation.tsx
@@ -15,7 +15,7 @@ import {
   Button,
   buttonVariants,
 } from '@rocket-house-productions/shadcn-ui';
-import { BugIcon } from 'lucide-react';
+import { ChevronRightIcon, MenuIcon } from 'lucide-react';
 import Link from 'next/link';
 import cn from 'classnames';
 import Image from 'next/image';
@@ -23,20 +23,21 @@ import ButtonDownloadPdf from '../button-download-pdf';
 
 interface CourseDebugNavigationProps {
   course: any;
+  role?: string;
 }
 
-export function CourseDebugNavigation({ course }: CourseDebugNavigationProps) {
+export function CourseDebugNavigation({ course, role }: CourseDebugNavigationProps) {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant={'default'} size={'sm'} className={'mb-4 border-2 border-white bg-pink-500'}>
-          <BugIcon className={'h-4 w-4'} />
-          <span className={'sr-only'}>Debug Navigation</span>
+          <MenuIcon className={'h-4 w-4'} />
+          <span className={'sr-only'}>Quick Menu</span>
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Debug Navigation</DialogTitle>
+          <DialogTitle>Quick Menu</DialogTitle>
         </DialogHeader>
 
         <DialogBody>
@@ -45,19 +46,17 @@ export function CourseDebugNavigation({ course }: CourseDebugNavigationProps) {
               {course.modules.map((module: any, idx: number) => (
                 <AccordionItem value={module.id} key={module.id}>
                   <AccordionTrigger>
-                    <div className={'flex w-full justify-between'}>
+                    <div
+                      className={`font-lesson-heading flex w-full justify-between`}
+                      style={{ color: module.color || 'black' }}>
                       <span>
-                        {idx + 1} {module.title}{' '}
+                        {idx + 1}. {module.title}{' '}
                       </span>
-                      <span
-                        className={'inline-block h-6 w-6 rounded-full'}
-                        style={{ backgroundColor: module.color || 'transparent' }}></span>
                     </div>
                   </AccordionTrigger>
                   <AccordionContent>
                     <div className={'flex flex-col space-y-3'}>
                       <div>
-                        <div className={'mb-4 font-bold'}>Description:</div>
                         <div dangerouslySetInnerHTML={{ __html: module.description || '' }}></div>
                       </div>
                       <div>
@@ -68,60 +67,71 @@ export function CourseDebugNavigation({ course }: CourseDebugNavigationProps) {
                               <Link
                                 className={cn(
                                   buttonVariants({ variant: 'default' }),
-                                  'w-full',
-                                  lesson.category?.name === 'Dr Rhythm' && '!bg-pink-900',
-                                  lesson?.category?.name === 'Practice' && '!bg-gray-500',
+                                  'w-full !text-left',
+                                  lesson.category?.name === 'Dr Rhythm' && 'hover:!bg-accent !bg-pink-900',
+                                  lesson?.category?.name === 'Practice' && 'hover:!bg-accent !bg-gray-500',
                                 )}
                                 href={`/courses/${course.slug}/modules/${module.slug}/lessons/${lesson.slug}`}>
-                                {idx + 1}: {lesson.category?.name} - {lesson.title}
+                                <div className={'flex w-full flex-row justify-between'}>
+                                  <span>
+                                    {idx + 1}. {lesson.title}
+                                  </span>
+                                  <i>
+                                    <ChevronRightIcon className={'h-5 w-5'} />
+                                  </i>
+                                </div>
                               </Link>
                             </li>
                           ))}
                         </ul>
                       </div>
-                      <div>
-                        <div className={'mb-4 font-bold'}>Attachments</div>
-                        <ul className={'space-y-3 text-sm'}>
-                          {module.attachments.map((attachment: any) => (
-                            <li key={attachment.id} className={'flex flex-col space-y-1.5 rounded-md border p-3'}>
-                              <div>Type: {attachment.attachmentType?.name}</div>
-                              <div>Name: {attachment.name}</div>
-                              <ButtonDownloadPdf url={attachment.url} filename={attachment.name || 'download'} />
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                      <div>
-                        <div>
-                          <div className={'mb-4 font-bold'}>Awards</div>
-                          <ul>
-                            {module.availableAwards.map((item: any, idx: number) => {
-                              return (
-                                <li key={'award' + item.awardType.id} className={'flex space-x-1.5'}>
-                                  {item.awardType.badgeUrl && (
-                                    <Image src={item.awardType.badgeUrl} width={70} height={70} alt={'badge'} />
-                                  )}{' '}
-                                  <span>{item.awardType.name}</span>
+                      {role === 'admin' && (
+                        <>
+                          <div>
+                            <div className={'mb-4 font-bold'}>Attachments</div>
+                            <ul className={'space-y-3 text-sm'}>
+                              {module.attachments.map((attachment: any) => (
+                                <li key={attachment.id} className={'flex flex-col space-y-1.5 rounded-md border p-3'}>
+                                  <div>Type: {attachment.attachmentType?.name}</div>
+                                  <div>Name: {attachment.name}</div>
+                                  <ButtonDownloadPdf url={attachment.url} filename={attachment.name || 'download'} />
                                 </li>
-                              );
-                            })}
-                          </ul>
-                        </div>
-                      </div>
-                      <div>
-                        <div>
-                          <div className={'mb-4 font-bold'}>Quiz</div>
-                          <ul>
-                            {module.lessons.map((item: any, idx: number) => {
-                              return (
-                                <li key={'quiz' + item.id}>
-                                  {idx + 1} {item.title} - {item.questionaries.length ? 'yes' : 'no'}
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </div>
-                      </div>
+                              ))}
+                            </ul>
+                          </div>
+                          <div>
+                            <div>
+                              <div className={'mb-4 font-bold'}>Awards</div>
+                              <ul>
+                                {module.availableAwards.map((item: any, idx: number) => {
+                                  return (
+                                    <li key={'award' + item.awardType.id} className={'flex space-x-1.5'}>
+                                      {item.awardType.badgeUrl && (
+                                        <Image src={item.awardType.badgeUrl} width={70} height={70} alt={'badge'} />
+                                      )}{' '}
+                                      <span>{item.awardType.name}</span>
+                                    </li>
+                                  );
+                                })}
+                              </ul>
+                            </div>
+                          </div>
+                          <div>
+                            <div>
+                              <div className={'mb-4 font-bold'}>Quiz</div>
+                              <ul>
+                                {module.lessons.map((item: any, idx: number) => {
+                                  return (
+                                    <li key={'quiz' + item.id}>
+                                      {idx + 1} {item.title} - {item.questionaries.length ? 'yes' : 'no'}
+                                    </li>
+                                  );
+                                })}
+                              </ul>
+                            </div>
+                          </div>
+                        </>
+                      )}
                     </div>
                   </AccordionContent>
                 </AccordionItem>
