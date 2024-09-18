e1c92fe0ab793e027697dc7acf1c6fd71a905742
Olaf Siebert <olaf@biffnewmedia.com>
2024-09-18 20:35:24 +0200

    feat: Award component added to list

--- libs/shared/lesson/src/lib/components/course/course-scene/module-awards.tsx
+++ libs/shared/lesson/src/lib/components/course/course-scene/module-awards.tsx
@@ -1,20 +1,44 @@
 import * as THREE from 'three';
-import { Plane, Text } from '@react-three/drei';
+import { Plane, Text, useTexture } from '@react-three/drei';
 import { ModulePosition } from './course.types';
+import { useModuleProgressStore } from '@rocket-house-productions/providers';
 
 interface ModuleAwardsProps {
   modulePosition: ModulePosition[];
   pathLength?: THREE.Vector3;
 }
 
+const AwardPlane = ({ image }: { image: string }) => {
+  const texture = useTexture(image);
+
+  if (texture) {
+    return (
+      <Plane args={[3, 3]} position={[-6, 0, 0]}>
+        <meshStandardMaterial map={texture} transparent={true} />
+      </Plane>
+    );
+  }
+};
+
 export function ModuleAwards({ modulePosition, pathLength }: ModuleAwardsProps) {
+  const awards = useModuleProgressStore(store => store.getAwards());
+
+  const getAwardImage = (moduleId: string) => {
+    const award = awards.find(award => award.moduleId === moduleId);
+    console.log('AWARD:', award?.awardType.badgeUrl);
+    console.log('AWARD: RENDER', awards);
+    try {
+      if (award?.awardType.badgeUrl) {
+        return <AwardPlane image={award.awardType.badgeUrl} />;
+      }
+    } catch (e) {
+      console.log('ERROR:', e);
+    }
+    return null;
+  };
+
   return modulePosition.map((item, idx) => (
-    <group position={modulePosition[idx + 1]?.position || pathLength}>
-      <Plane args={[2, 2]} position={[-10, 0, 0]} />
-      <Text color="black" scale={0.3} anchorX="center" anchorY="middle" position={[-5, 0, 0.3]} castShadow={true}>
-        Award: {item.name}
-      </Text>
-    </group>
+    <group position={modulePosition[idx + 1]?.position || pathLength}>{getAwardImage(item.id)}</group>
   ));
 }
 
