cf650c3a0d3422be37838c0af93ecca295c08f62
Olaf Siebert <olaf@biffnewmedia.com>
2024-09-24 10:12:25 +0200

    fix: db error export default server action turned into client

--- libs/shared/integration/src/index.ts
+++ libs/shared/integration/src/index.ts
@@ -1,5 +1,5 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
 export { stripe } from './stripe';
-export { stripeCheckoutAction, stripeCheckout, stripeCheckoutSessionStatus } from './stripe-checkout';
+export { stripeCheckout, stripeCheckoutSessionStatus } from './stripe-checkout';
 export * from './db';
