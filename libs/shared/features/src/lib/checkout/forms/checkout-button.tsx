10ea6d5e4fdd3f8a0bd5080a5cb57451a0fb710e
Olaf Siebert <olaf@biffnewmedia.com>
2024-09-25 20:27:42 +0200

    fix:  redirect error fix

---
+++ libs/shared/features/src/lib/checkout/forms/checkout-button.tsx
@@ -0,0 +1,32 @@
+import cn from 'classnames';
+import { Loader2Icon } from 'lucide-react';
+import { Button } from '@rocket-house-productions/shadcn-ui';
+import React from 'react';
+import { useFormStatus } from 'react-dom';
+
+interface CheckoutButtonProps {
+  mostPopular?: boolean;
+}
+
+export function CheckoutButton({ mostPopular }: CheckoutButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type={'submit'}
+      variant={mostPopular ? 'default' : 'outline'}
+      className={cn('mt-6 w-full shadow-sm shadow-black/30')}
+      size={'lg'}
+      disabled={pending}>
+      {pending ? (
+        <i className={'mr-3'}>
+          <Loader2Icon className={'h-5 w-5 animate-spin'} />
+        </i>
+      ) : (
+        'Start now'
+      )}
+    </Button>
+  );
+}
+
+export default CheckoutButton;
