79748dcae117ba0760f2e6462f7266e34ac574f4
Olaf Siebert <olaf@biffnewmedia.com>
2024-10-07 14:31:11 +0200

    fix: order page scroll fixed

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/layout.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/layout.tsx
@@ -5,10 +5,25 @@ import { BASE_URL } from './_component/path-types';
 import { OnBoardingContextProvider } from './_component/onBoardinglContext';
 import { NavbarSimple } from '@rocket-house-productions/layout';
 import logo from '@assets/logo.png';
+import { Viewport } from 'next';
+import { headers } from 'next/headers';
 
 interface LayoutProps {
   children: React.ReactNode;
   params: { purchaseId: string };
+}
+
+export async function generateViewport(): Promise<Viewport> {
+  const userAgent = headers().get('user-agent');
+  const isiPhone = /iphone/i.test(userAgent ?? '');
+  return isiPhone
+    ? {
+        width: 'device-width',
+        initialScale: 1,
+        userScalable: false,
+        maximumScale: 1, // disables auto-zoom on ios safari
+      }
+    : {};
 }
 
 export default async function Layout({ children, params }: LayoutProps) {
