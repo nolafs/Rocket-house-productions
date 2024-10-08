c46edd7b157b156e9fab002eedddcbfe0c165c53
Olaf Siebert <olaf@biffnewmedia.com>
2024-10-08 20:03:25 +0200

    feat: add animation to footer ui of course page

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(lesson)/[slug]/(course)/_components/courseNavigationPage.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(lesson)/[slug]/(course)/_components/courseNavigationPage.tsx
@@ -6,8 +6,10 @@ import {
   ModuleAttachments,
 } from '@rocket-house-productions/lesson';
 import { Course } from '@prisma/client';
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import dynamic from 'next/dynamic';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const CourseNavigation = dynamic(
   () => import('@rocket-house-productions/lesson').then(module => module.CourseNavigation),
@@ -22,31 +24,39 @@ interface CourseNavigationPageProps {
 }
 
 export function CourseNavigationPage({ course, role, childId = null, purchaseType = null }: CourseNavigationPageProps) {
-  const [ready, setReady] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
-  const handleLoaded = (loaded: boolean) => {
-    setReady(loaded);
-  };
+  const { contextSafe } = useGSAP(
+    () => {
+      gsap.set('.footer-ui', { autoAlpha: 0, yPercent: 100 });
+    },
+    { scope: containerRef },
+  );
+
+  const handleLoaded = contextSafe((ready: boolean) => {
+    console.log('[CourseNavigationPage] handleLoaded', ready);
+
+    if (ready) {
+      gsap.to('.footer-ui', { duration: 1, autoAlpha: 1, yPercent: 0 });
+    } else {
+      gsap.to('.footer-ui', { duration: 1, autoAlpha: 0, yPercent: 100 });
+    }
+  });
 
   return (
-    <>
+    <div ref={containerRef}>
       <div id="course-nav" className={'relative h-screen w-full'}>
         <CourseNavigation purchaseType={purchaseType} course={course} onLoaded={loaded => handleLoaded(loaded)} />
       </div>
-      {ready && (
-        <div
-          className={
-            'fixed bottom-2 left-0 z-50 flex w-full flex-col justify-center gap-y-5 px-3 md:flex-row md:justify-between'
-          }>
-          <LessonCourseProgression course={course} />
-          <div className={'flex items-center justify-center gap-x-2'}>
-            <CourseLeaderboard courseId={course.id} childId={childId} />
-            <ModuleAttachments course={course} purchaseType={purchaseType} />
-            <CourseQuickNavigation course={course} role={role} />
-          </div>
+      <div className="footer-ui fixed bottom-2 left-0 z-50 flex w-full flex-col justify-center gap-y-5 px-3 md:flex-row md:justify-between">
+        <LessonCourseProgression course={course} />
+        <div className={'flex items-center justify-center gap-x-2'}>
+          <CourseLeaderboard courseId={course.id} childId={childId} />
+          <ModuleAttachments course={course} purchaseType={purchaseType} />
+          <CourseQuickNavigation course={course} role={role} />
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
 
