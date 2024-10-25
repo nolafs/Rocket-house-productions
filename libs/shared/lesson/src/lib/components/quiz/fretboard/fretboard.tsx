7ecb0216636477555c9f2c86389f14f4de2c642b
Olaf Siebert <olaf@biffnewmedia.com>
2024-10-25 09:45:36 +0200

    fix: Quiz
    - button image layout equal height

--- libs/shared/lesson/src/lib/components/quiz/fretboard/fretboard.tsx
+++ libs/shared/lesson/src/lib/components/quiz/fretboard/fretboard.tsx
@@ -58,12 +58,12 @@ const Fretboard = forwardRef<FretboardHandleProps, FretboardProps>(
 
     useGSAP(
       () => {
+        if (!fretboardRef.current) return;
+
         const dragItems: HTMLDivElement[] = gsap.utils.toArray('#drag-items .drag-item ');
         const dropZones: HTMLDivElement[] = gsap.utils.toArray('.dropzone');
 
-        if (dragItems.length === 0) return;
-        if (!fretboardRef.current) return;
-        if (!dropZones.length) return;
+        if (dragItems.length === 0 || !dropZones.length) return;
 
         const dragSetup = () => {
           for (let i = 0; i <= dragItems.length; i++) {
