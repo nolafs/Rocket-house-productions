b9aec28db6fe7b38bc309588acc5b06037a13853
Olaf Siebert <olaf@biffnewmedia.com>
2024-11-08 11:58:13 +0100

    fix: move course access check to middleware

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/page.tsx
@@ -1,11 +1,6 @@
-import { auth } from '@clerk/nextjs/server';
 import { Loader2 } from 'lucide-react';
 
 export default async function Page({ params }: { params: { product: string[] } }) {
-  const { userId, sessionClaims } = auth();
-
-  console.log('[COURSE]', sessionClaims);
-
   return (
     <div className={'mt-5 flex h-svh w-full flex-col items-center justify-center'}>
       <Loader2 className={'text-primary h-12 w-12 animate-spin'} />
