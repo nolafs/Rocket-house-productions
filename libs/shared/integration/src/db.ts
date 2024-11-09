05cecadc938ebd5bf69f78dadd5595068988d9c1
Olaf Siebert <olaf@biffnewmedia.com>
2024-11-09 11:28:49 +0100

    fix: database connection error

--- libs/shared/integration/src/db.ts
+++ libs/shared/integration/src/db.ts
@@ -11,8 +11,8 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+export const db = prismaClientSingleton(); //globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default db;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db;
+//if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db;
