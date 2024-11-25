00d33487e9853d7fb6fb9d92a4ed217dd6f5e5b4
Olaf Siebert <olaf@biffnewmedia.com>
2024-11-25 09:06:01 +0100

    fix: middleware linking not working on live

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/step-three/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/step-three/page.tsx
@@ -4,7 +4,7 @@ import { BASE_URL } from '../_component/path-types';
 import { createClient } from '@/prismicio';
 
 export default async function StepTwo({ params }: { params: { purchaseId: string } }) {
-  const baseUrl = `${BASE_URL}/${params.purchaseId}`;
+  const baseUrl = `${BASE_URL}${params.purchaseId}`;
 
   const client = createClient();
   const { data } = await client.getSingle('onboarding');
