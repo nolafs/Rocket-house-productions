406797d015be94d58a5bbad7e4ce92b504090fb8
Olaf Siebert <olaf@biffnewmedia.com>
2024-11-25 09:06:01 +0100

    fix: middleware linking not working on live

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 import { BASE_URL } from './_component/path-types';
 
 export default async function Page({ params }: { params: { purchaseId: string } }) {
-  const destination = `${BASE_URL}/${params.purchaseId}/intro`;
+  const destination = `${BASE_URL}${params.purchaseId}/intro`;
 
   // Redirect immediately on server-side if necessary
   redirect(destination);
