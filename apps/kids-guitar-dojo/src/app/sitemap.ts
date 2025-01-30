2cb8b1c5af0c1c57ff98a5f9af4e535db9404538
Olaf Siebert <olaf@biffnewmedia.com>
2025-01-30 10:18:29 +0100

    feat: Add blog posts to SITEMAP

--- apps/kids-guitar-dojo/src/app/sitemap.ts
+++ apps/kids-guitar-dojo/src/app/sitemap.ts
@@ -1,6 +1,34 @@
 import { MetadataRoute } from 'next';
+import { createClient } from '@/prismicio';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const client = createClient();
+
+  const posts = await client
+    .getByType('blog_post', {
+      pageSize: 100,
+      page: 0,
+      orderings: [
+        {
+          field: 'my.posts.published_date',
+          direction: 'desc',
+        },
+      ],
+    })
+    .then(response => {
+      return response.results;
+    })
+    .catch(() => []);
+
+  const blogPosts = posts.map(post => {
+    return {
+      url: `https://${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.uid}`,
+      lastModified: post.data.publishing_date?.toString() ?? new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    };
+  });
+
   return [
     {
       url: `https://${process.env.NEXT_PUBLIC_BASE_URL}`,
@@ -32,5 +60,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-  ];
+    ...blogPosts,
+  ] as MetadataRoute.Sitemap;
 }
