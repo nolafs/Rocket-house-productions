876c9651ebc870d494b34fa9524ecec09accd9d1
Olaf Siebert <olaf@biffnewmedia.com>
2025-02-07 16:43:34 +0100

    feat: contact form update

--- libs/shared/actions/src/verifiy-captcha.ts
+++ libs/shared/actions/src/verifiy-captcha.ts
@@ -1,3 +1,4 @@
+'use server';
 export async function VerifyCaptcha(token: string) {
   const response = await fetch(
     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
