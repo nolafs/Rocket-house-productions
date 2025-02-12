758b63f0f5f52228e309ef6e3ec32ba8d4db0981
Olaf Siebert <olaf@biffnewmedia.com>
2025-02-12 16:02:44 +0100

    chore: update
    - search engine scheme added to blog post
    - canonical link added

--- apps/kids-guitar-dojo/src/prismicio.ts
+++ apps/kids-guitar-dojo/src/prismicio.ts
@@ -1,6 +1,7 @@
 import * as prismic from '@prismicio/client';
 import * as prismicNext from '@prismicio/next';
 import config from '../slicemachine.config.json';
+import { ClientConfig } from '@prismicio/client';
 
 /**
  * The project's Prismic repository name.
@@ -29,7 +30,7 @@ const routes: prismic.ClientConfig['routes'] = [
  *
  * @param config - Configuration for the Prismic client.
  */
-export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
+export const createClient = (config: ClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
     fetchOptions:
@@ -39,11 +40,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
     ...config,
   });
 
-  prismicNext.enableAutoPreviews({
-    client,
-    previewData: config.previewData,
-    req: config.req,
-  });
+  prismicNext.enableAutoPreviews({ client });
 
   return client;
 };
