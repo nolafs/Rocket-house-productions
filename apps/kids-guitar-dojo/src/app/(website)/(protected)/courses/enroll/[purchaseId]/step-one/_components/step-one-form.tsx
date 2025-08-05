52548dbbb403a3fdded44f0854fbd7e0feb45498
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-05 14:07:35 +0200

    chore: update to nextjs 15 and react 19

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/step-one/_components/step-one-form.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/step-one/_components/step-one-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { DialogLayout } from '@rocket-house-productions/lesson';
 import { FormErrors } from '../../_component/path-types';
+/*
 import { PrevButton } from '../../_component/button-prev';
 import { useForm } from 'react-hook-form';
 import stepOneFormAction from '../action';
@@ -22,9 +23,13 @@ import { useEffect, useRef, useActionState } from 'react';
 import { XIcon } from 'lucide-react';
 import { useOnBoardingContext } from '../../_component/onBoardinglContext';
 import ButtonSubmit from '../../_component/button-submit';
+
+import { useMenuActive } from '@/app/(website)/(protected)/courses/enroll/[purchaseId]/_component/useMenuActive';
+
+ */
+import { isFilled } from '@prismicio/client';
 import { KeyTextField, RichTextField } from '@prismicio/types';
 import { PrismicRichText } from '@prismicio/react';
-import { useMenuActive } from '@/app/(website)/(protected)/courses/enroll/[purchaseId]/_component/useMenuActive';
 
 const initialState: FormErrors = {};
 
@@ -40,36 +45,13 @@ interface StepOneFormProps {
     };
   };
   header?: KeyTextField | string | null | undefined;
-  body?: RichTextField | string | null | undefined;
+  body?: RichTextField;
 }
 
 export default function StepOneForm({ baseUrl, purchase, header, body }: StepOneFormProps) {
-  const [serverError, formAction] = useActionState(stepOneFormAction, initialState);
-  const { updateOnBoardingDetails, onBoardingData } = useOnBoardingContext();
-  const setActive = useMenuActive(state => state.setActive);
-  const formRef = useRef<HTMLFormElement>(null);
-  useEffect(() => {
-    setActive(true);
-  }, []);
-
-  const form = useForm<z.infer<typeof stepOneSchema>>({
-    resolver: zodResolver(stepOneSchema),
-    defaultValues: {
-      firstName: onBoardingData.firstName || purchase?.account.firstName || '',
-      lastName: onBoardingData.lastName || purchase?.account.lastName || '',
-      email: onBoardingData.email || purchase?.account.email || '',
-      confirmTerms: onBoardingData.confirmTerms || false,
-      parentConsent: onBoardingData.parentConsent || false,
-      newsletter: onBoardingData.newsletter || false,
-      notify: onBoardingData.notify || false,
-      productId: baseUrl,
-    },
-  });
-
   return (
     <DialogLayout title={header || "🎸 Parent's Jam Session 🎸"}>
-      {body && <div className="body">{typeof body === 'string' ? body : <PrismicRichText field={body} />}</div>}
-      <div className={'flex-1 text-left'}>test</div>
+      {isFilled.richText(body) && <div className="body">{<PrismicRichText field={body} />}</div>}
     </DialogLayout>
   );
 }
