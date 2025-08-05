1dba069a1a1672af02d78f8068427f7700f4da7d
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-05 12:48:21 +0200

    chore: update to nextjs 15 and react 19

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/_component/onBoardinglContext.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/enroll/[purchaseId]/_component/onBoardinglContext.tsx
@@ -54,16 +54,22 @@ export const OnBoardingContextProvider = ({ children }: { children: React.ReactN
   );
 
   const saveDataToLocalStorage = (currentDealData: OnBoardingInitialValuesType) => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(currentDealData));
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(currentDealData));
+    }
   };
 
   const readFromLocalStorage = () => {
-    const loadedDataString = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (!loadedDataString) return setOnBoardingData(defaultOnBoarding);
-    const validated = onBoardingInitialValuesSchema.safeParse(JSON.parse(loadedDataString));
+    if (typeof window !== 'undefined') {
+      const loadedDataString = localStorage.getItem(LOCAL_STORAGE_KEY);
+      if (!loadedDataString) return setOnBoardingData(defaultOnBoarding);
+      const validated = onBoardingInitialValuesSchema.safeParse(JSON.parse(loadedDataString));
 
-    if (validated.success) {
-      setOnBoardingData(validated.data);
+      if (validated.success) {
+        setOnBoardingData(validated.data);
+      } else {
+        setOnBoardingData(defaultOnBoarding);
+      }
     } else {
       setOnBoardingData(defaultOnBoarding);
     }
