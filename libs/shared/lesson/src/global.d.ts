028996aa5c257d59b556437114ac02c9c8a680c0
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-09 14:27:41 +0200

    chore: update to nextjs 15 and react 19

--- libs/shared/lesson/src/global.d.ts
+++ libs/shared/lesson/src/global.d.ts
@@ -1,9 +1,11 @@
-import { Object3DNode } from '@react-three/fiber';
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+import { Object3DNode, MaterialNode } from '@react-three/fiber';
 
 declare module '@react-three/fiber' {
   interface ThreeElements {
     meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
-    meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
+    meshLineMaterial: MaterialNode<MeshLineMaterial, typeof MeshLineMaterial>;
   }
 }
+
+export {};
