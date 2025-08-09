e2b05d64a1d4014e0fd40690c4bd79995ba8cf62
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-09 15:26:32 +0200

    chore: update to nextjs 15 and react 19

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(lesson)/[slug]/modules/[module_slug]/lessons/[lesson_slug]/_components/lessonComponent.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(lesson)/[slug]/modules/[module_slug]/lessons/[lesson_slug]/_components/lessonComponent.tsx
@@ -1,7 +1,17 @@
 'use client';
 import { SectionCourse, SectionLesson, SectionModule } from '@rocket-house-productions/types';
-import { LessonContent } from '@rocket-house-productions/lesson/server';
-import { LessonBookCta, LessonNext, LessonVideo } from '@rocket-house-productions/lesson';
+import { LessonBookCta, LessonNext } from '@rocket-house-productions/lesson';
+import { useEffect } from 'react';
+
+import dynamic from 'next/dynamic';
+
+const LessonContent = dynamic(() => import('@rocket-house-productions/lesson').then(mod => mod.LessonContent), {
+  ssr: false,
+});
+
+const LessonVideo = dynamic(() => import('@rocket-house-productions/lesson').then(mod => mod.LessonVideo), {
+  ssr: false,
+});
 
 interface LessonComponentProps {
   data: { course: any; module: any; lesson: any };
@@ -10,11 +20,13 @@ interface LessonComponentProps {
 }
 
 export function LessonComponent({ data, child, page }: LessonComponentProps) {
-  if (typeof window !== 'undefined') {
-    console.info('Window is defined here');
-  } else {
-    console.warn('Window is undefined here');
-  }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      console.info('Window is defined here');
+    } else {
+      console.warn('Window is undefined here');
+    }
+  }, []);
 
   return (
     <>
