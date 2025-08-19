25548c1c0ef0efe5c88fbf4dbafb55c26e828eb2
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-19 18:54:43 +0200

    feat: update lint

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/page.tsx
@@ -1,6 +1,6 @@
 import { Loader2 } from 'lucide-react';
 
-export default async function Page() {
+export default function Page() {
   return (
     <div className={'mt-5 flex h-svh w-full flex-col items-center justify-center'}>
       <Loader2 className={'text-primary h-12 w-12 animate-spin'} />
