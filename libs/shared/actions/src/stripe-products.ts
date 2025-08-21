75daa10115d438f8ac77f305b0c344f62523a6bc
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-21 14:29:42 +0200

    feat: admin access hack for courses

--- libs/shared/actions/src/stripe-products.ts
+++ libs/shared/actions/src/stripe-products.ts
@@ -2,6 +2,8 @@
 import { stripe } from '@rocket-house-productions/integration/server';
 
 export const stripePrices = async (productId: string, sale = false) => {
+  console.log(`Stripe prices for ${productId}`);
+
   const productPrice = await stripe.prices.search({
     query: `product:'${productId}' AND active:'true'`,
   });
