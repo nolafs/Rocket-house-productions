e29a2d5b9e0bb285e4cee7707234a41eef0cf37f
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-22 10:28:24 +0200

    refract: clean up move some into server package

--- libs/shared/store/src/index.ts
+++ libs/shared/store/src/index.ts
@@ -1,7 +1,5 @@
-// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-
+'use client';
 export { usePurchaseStore } from './use-purchase-store';
-
 export type {
   ModuleProgressStore,
   ModuleProgression,
