8a1a417f2d68dbbcd55963f3aa92f0433ca883b6
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-24 09:06:55 +0200

    refract: clean up move some into server package

--- libs/shared/actions/src/server.ts
+++ libs/shared/actions/src/server.ts
@@ -1,4 +1,3 @@
-// Use this file to export React server components
 export { stripePrices } from './stripe-products';
 export { checkoutUrl } from './stripe-checkout-url';
 export { getAccount } from './get-account';
--- libs/shared/actions/src/stripe-products.ts
+++ libs/shared/actions/src/stripe-products.ts
@@ -2,13 +2,9 @@
 import { stripe } from '@rocket-house-productions/integration/server';
 
 export const stripePrices = async (productId: string, sale = false) => {
-  console.log(`Stripe prices for ${productId}`);
-
   const productPrice = await stripe.prices.search({
     query: `product:'${productId}' AND active:'true'`,
   });
 
   return productPrice.data;
 };
-
-export default stripePrices;
