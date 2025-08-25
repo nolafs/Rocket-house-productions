c858039679705e5e4dc11def3d7948145425a890
Olaf Siebert <olaf@biffnewmedia.com>
2025-08-25 10:31:51 +0200

    feat: Price stripe development mode sandbox

--- libs/shared/actions/src/stripe-products.ts
+++ libs/shared/actions/src/stripe-products.ts
@@ -2,9 +2,10 @@
 import { stripe } from '@rocket-house-productions/integration/server';
 
 export const stripePrices = async (productId: string, sale = false) => {
-  const productPrice = await stripe.prices.search({
-    query: `product:'${productId}' AND active:'true'`,
+  const { data } = await stripe.prices.list({
+    product: productId,
+    active: true,
+    limit: 100,
   });
-
-  return productPrice.data;
+  return data;
 };
