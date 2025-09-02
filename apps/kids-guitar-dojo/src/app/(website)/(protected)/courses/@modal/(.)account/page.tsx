43a931084fa5fe86084a011a128a2a76809ec901
Olaf Siebert <olaf@biffnewmedia.com>
2025-09-02 14:13:33 +0200

    feat: update user profile account pages

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/@modal/(.)account/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/@modal/(.)account/page.tsx
@@ -1,7 +1,8 @@
 import { DialogLayout } from '@rocket-house-productions/lesson';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-import { UserProfile } from '@clerk/nextjs';
+import { ClerkProvider, UserProfile } from '@clerk/nextjs';
+import { Suspense } from 'react';
 
 export default async function Page() {
   const { userId, sessionClaims } = await auth();
@@ -16,7 +17,11 @@ export default async function Page() {
   return (
     <div className={'fixed left-0 top-0 z-30 flex h-full w-full items-center justify-center bg-black/50'}>
       <DialogLayout hasCloseUrl={'/courses'} title={`🎸 Account - ${name} 🎸 `} classNames={'px-0 pb-0 pt-0 md:px-0'}>
-        <UserProfile />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ClerkProvider dynamic>
+            <UserProfile />
+          </ClerkProvider>
+        </Suspense>
       </DialogLayout>
     </div>
   );
--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/account/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/account/page.tsx
@@ -1,12 +1,17 @@
 import { LessonPageWrapper } from '@rocket-house-productions/lesson/server';
-import { UserProfile } from '@clerk/nextjs';
+import { ClerkProvider, UserProfile } from '@clerk/nextjs';
 import { DialogReturnUrl } from '@rocket-house-productions/lesson';
+import { Suspense } from 'react';
 
 export default function Page() {
   return (
     <LessonPageWrapper>
       <DialogReturnUrl title={'Parent Account'}>
-        <UserProfile />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ClerkProvider dynamic>
+            <UserProfile />
+          </ClerkProvider>
+        </Suspense>
       </DialogReturnUrl>
     </LessonPageWrapper>
   );
