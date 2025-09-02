adaa3dd2252d43890b45561b1045923328663811
Olaf Siebert <olaf@biffnewmedia.com>
2025-09-02 17:42:28 +0200

    feat: Membership admin
    - add endpoint for app setting to return data for pricing tables
    - action for getting app settings
    - Dashboard card to access membership edit on course

--- libs/shared/actions/src/server.ts
+++ libs/shared/actions/src/server.ts
@@ -22,3 +22,4 @@ export { updateUserRole } from './update-user-role';
 export { deleteUser } from './delete-user';
 export { SessionFlags } from './session';
 export { HandleSubscription } from './subscription-action';
+export { getAppSettings } from './get-app-settings';
