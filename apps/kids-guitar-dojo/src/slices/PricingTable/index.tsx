14600f6616528ee3da0de1f846b194b11d446fba
Olaf Siebert <olaf@biffnewmedia.com>
2025-09-03 07:34:38 +0200

    fix: Course card wrong price cards stripe id for dev

--- apps/kids-guitar-dojo/src/slices/PricingTable/index.tsx
+++ apps/kids-guitar-dojo/src/slices/PricingTable/index.tsx
@@ -3,8 +3,10 @@ import { SliceComponentProps } from '@prismicio/react';
 import { Bounded } from '@components/Bounded';
 import { SectionPricingTable } from '@rocket-house-productions/features';
 import { createClient } from '@/prismicio';
-import { AllDocumentTypes } from '../../../prismicio-types';
+import { AllDocumentTypes } from '@/prismic-types';
 import { Tier } from '@rocket-house-productions/types';
+import { getAppSettings } from '@rocket-house-productions/actions/server';
+
 /**
  * Props for `PricingTable`.
  */
@@ -17,11 +19,36 @@ const PricingTable = async ({ slice }: PricingTableProps) => {
   const client = createClient();
   const tierPricing: AllDocumentTypes[] = [];
 
+  const appSettings = await getAppSettings();
+
+  console.log('appSettings', appSettings);
+
   for (const item of slice.primary.tiers) {
     if (item.tier) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-expect-error
       const tier = await client.getByID(item.tier?.id);
+      const pricingIds: {
+        stripeProductId: string | null;
+        stripe_productid_dev: string | null;
+      } = {
+        stripeProductId: null,
+        stripe_productid_dev: null,
+      };
+
+      if (tier.uid === 'standard') {
+        // Always exclude the basic tier
+        pricingIds.stripe_productid_dev = appSettings?.membershipSettings?.course?.stripeProductStandardIdDev || null;
+        pricingIds.stripeProductId = appSettings?.membershipSettings?.course?.stripeProductStandardId || null;
+      }
+
+      if (tier.uid === 'premium') {
+        pricingIds.stripe_productid_dev = appSettings?.membershipSettings?.course?.stripeProductPremiumIdDev || null;
+        pricingIds.stripeProductId = appSettings?.membershipSettings?.course?.stripeProductPremiumId || null;
+      }
+
+      tier.data = { ...tier.data, ...pricingIds };
+
       tierPricing.push(tier);
     }
   }
