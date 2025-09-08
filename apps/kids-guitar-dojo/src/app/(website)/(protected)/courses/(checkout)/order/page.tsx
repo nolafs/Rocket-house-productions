d17299fa1f2574ead7c08a99762fd59598205845
Olaf Siebert <olaf@biffnewmedia.com>
2025-09-08 10:50:16 +0200

    feat: new pricing table

--- apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(checkout)/order/page.tsx
+++ apps/kids-guitar-dojo/src/app/(website)/(protected)/courses/(checkout)/order/page.tsx
@@ -2,8 +2,6 @@ import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import PurchaseOption from './_components/purchase-option';
 import { SectionPricingTable } from '@rocket-house-productions/features';
-import { getAppSettings } from '@rocket-house-productions/actions/server';
-import { Tier } from '@prisma/client';
 
 export default async function Page() {
   const { userId, sessionClaims } = await auth();
@@ -18,23 +16,21 @@ export default async function Page() {
 
   if (sessionClaims.metadata?.status === 'active') {
     if (sessionClaims.metadata?.type === 'free') {
-      console.info('User is already active and has a free (.)account');
+      console.info('User is already active and has a free account');
       redirect('/courses/upgrade');
     }
 
     if (sessionClaims.metadata?.type === 'paid') {
-      console.info('User is already active and has a paid (.)account');
+      console.info('User is already active and has a paid account');
       redirect('/courses');
     }
   }
-
-  const appSettings = await getAppSettings();
 
   return (
     <main>
       <PurchaseOption userId={userId} email={sessionClaims?.email as string}>
         <section className={'container px-5'}>
-          <SectionPricingTable tiers={appSettings?.membershipSettings?.course.tiers as Tier[]} checkout={true} />
+          <SectionPricingTable checkout={true} />
         </section>
       </PurchaseOption>
     </main>
