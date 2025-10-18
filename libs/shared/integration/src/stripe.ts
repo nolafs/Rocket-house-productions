04cccd91c03eb262c3cfdc2d20594940a7596ac1
Olaf Siebert <olaf@biffnewmedia.com>
2025-10-18 13:10:47 +0200

    feat: Packages updated

--- libs/shared/integration/src/stripe.ts
+++ libs/shared/integration/src/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2025-08-27.basil',
+  apiVersion: '2025-09-30.clover',
   appInfo: {
     name: 'kid-guitar-dojo',
     url: 'https://kidguitardojo.com/',
