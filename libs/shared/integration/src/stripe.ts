c15c7b00c646a79e7d02e1710c4c511fefc7b115
Olaf Siebert <olaf@biffnewmedia.com>
2026-01-18 12:35:51 +0100

    fix: Stripe API version update

--- libs/shared/integration/src/stripe.ts
+++ libs/shared/integration/src/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2025-11-17.clover',
+  apiVersion: '2025-12-15.clover',
   appInfo: {
     name: 'kid-guitar-dojo',
     url: 'https://kidguitardojo.com/',
