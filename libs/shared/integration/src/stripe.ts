3e0e56505933ddfd8be863c02512e3b9efd69e7a
Olaf Siebert <olaf@biffnewmedia.com>
2026-02-01 13:23:35 +0100

    fix: pin verification 404 page on upgrade fixed

--- libs/shared/integration/src/stripe.ts
+++ libs/shared/integration/src/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2025-12-15.clover',
+  apiVersion: '2026-01-28.clover',
   appInfo: {
     name: 'kid-guitar-dojo',
     url: 'https://kidguitardojo.com/',
