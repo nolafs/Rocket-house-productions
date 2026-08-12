d0cf802f5a009016ce762014dab4c2c5ed826239
Olaf Siebert <olaf@biffnewmedia.com>
2026-08-12 14:13:30 +0200

    feat: Marketing Discount Slices

--- apps/kids-guitar-dojo/src/slices/Discounts/index.tsx
+++ apps/kids-guitar-dojo/src/slices/Discounts/index.tsx
@@ -1,6 +1,9 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { FC } from 'react';
+import { Content } from '@prismicio/client';
+import { SliceComponentProps } from '@prismicio/react';
+import { Bounded } from '@components/Bounded';
+import { SectionTitle } from '@rocket-house-productions/features';
+import { ButtonGroup } from '@rocket-house-productions/ui';
 
 /**
  * Props for `Discounts`.
@@ -11,16 +14,21 @@ export type DiscountsProps = SliceComponentProps<Content.DiscountsSlice>;
  * Component for "Discounts" Slices.
  */
 const Discounts: FC<DiscountsProps> = ({ slice }) => {
-	return (
-		<section
-			data-slice-type={slice.slice_type}
-			data-slice-variation={slice.variation}
-		>
-			Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-			<br />
-			<strong>You can edit this slice directly in your code editor.</strong>
-		</section>
-	)
+  const section_title = {
+    title: slice.primary.heading,
+    subtitle: slice.primary.subheading,
+  };
+
+  return (
+    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+      <Bounded as={'div'} yPadding={'md'} className="max-w-8xl mx-auto text-center">
+        {section_title && (
+          <SectionTitle {...section_title} description={slice.primary.body} align="center" titleSize="large" />
+        )}
+        {/* eslint-disable-next-line react/jsx-no-undef */}
+      </Bounded>
+    </section>
+  );
 };
 
-export default Discounts
+export default Discounts;
