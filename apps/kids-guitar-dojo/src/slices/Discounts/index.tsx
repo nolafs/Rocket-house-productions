3c27c864949bd91a465d103cb02f040fda00e736
Olaf Siebert <olaf@biffnewmedia.com>
2026-08-12 14:29:39 +0200

    feat: Marketing Discount Slices

--- apps/kids-guitar-dojo/src/slices/Discounts/index.tsx
+++ apps/kids-guitar-dojo/src/slices/Discounts/index.tsx
@@ -1,9 +1,8 @@
 import { FC } from 'react';
-import { Content } from '@prismicio/client';
+import { Content, isFilled } from '@prismicio/client';
 import { SliceComponentProps } from '@prismicio/react';
 import { Bounded } from '@components/Bounded';
 import { SectionTitle } from '@rocket-house-productions/features';
-import { ButtonGroup } from '@rocket-house-productions/ui';
 
 /**
  * Props for `Discounts`.
@@ -19,16 +18,32 @@ const Discounts: FC<DiscountsProps> = ({ slice }) => {
     subtitle: slice.primary.subheading,
   };
 
-  return (
-    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-      <Bounded as={'div'} yPadding={'md'} className="max-w-8xl mx-auto text-center">
-        {section_title && (
-          <SectionTitle {...section_title} description={slice.primary.body} align="center" titleSize="large" />
-        )}
-        {/* eslint-disable-next-line react/jsx-no-undef */}
-      </Bounded>
-    </section>
-  );
+  if (isFilled.contentRelationship(slice.primary.discount_code)) {
+    const discountCode = slice.primary.discount_code.data as Content.DiscountCodesDocumentData | undefined;
+
+    if (discountCode?.start_date) {
+      const isStarted = new Date() < new Date(discountCode.start_date);
+      if (isStarted) return null;
+    }
+
+    if (discountCode?.expiring_date) {
+      const isExpired = new Date() > new Date(discountCode.expiring_date);
+      if (isExpired) return null;
+    }
+
+    return (
+      <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+        <Bounded as={'div'} yPadding={'md'} className="max-w-8xl mx-auto text-center">
+          {section_title && (
+            <SectionTitle {...section_title} description={slice.primary.body} align="center" titleSize="large" />
+          )}
+          {/* eslint-disable-next-line react/jsx-no-undef */}
+        </Bounded>
+      </section>
+    );
+  }
+
+  return null;
 };
 
 export default Discounts;
